460ed1bef6e591eaf91c50bed8ddf07e96a094a6
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-08-21 19:07:12 +0900

    Add parameters and docstrings to function

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,7 +1,18 @@
 __version__ = "0.0.1"
 
 
-def func():
+def func(a, b=1, c=2):
+    """Test function.
+
+    Args:
+        a (int): Parameter A.
+        b (int, optional): Parameter B.
+        c (int, optional): Parameter C.
+
+    Returns:
+        This function returns nothing.
+
+    """
     pass
 
 
