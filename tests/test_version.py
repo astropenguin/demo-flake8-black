1e97e08d2a3ff0ca286467e9778329aa677d58ee
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-08-22 16:42:17 +0900

    Update test script

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,5 +2,12 @@ import demo
 
 
 def test_version():
-    """Ensure that the version is valid."""
+    """Make sure the version is valid."""
     assert demo.__version__ == "0.0.1"
+
+
+def test_func():
+    """Make sure the return of func is valid."""
+    assert demo.func(1) == 1
+    assert demo.func(1, 1) == 2
+    assert demo.func(1, 1, 1) == 3
